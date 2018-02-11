c1ed1db4611fb34731050e42867193cd3e6f0a83
arvindve <arvindcse.verma@gmail.com>
2018-02-11 11:18:29 +0800

    in-memory active mq example

---
+++ src/main/java/com/example/activemq/inmemoryactivemq/InmemoryActivemqApplication.java
@@ -0,0 +1,12 @@
+package com.example.activemq.inmemoryactivemq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmemoryActivemqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InmemoryActivemqApplication.class, args);
+	}
+}
